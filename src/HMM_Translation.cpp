a28c3599f015551648187c1f90568f2a6516adc1
dery-hit <zdr727@gmail.com>
2016-06-10 14:33:19 +0200

    add the structure definition of alignment and lexicon tables

--- src/HMM_Translation.cpp
+++ src/HMM_Translation.cpp
@@ -9,7 +9,21 @@
 #include <iostream>
 using namespace std;
 
+struct alignmentTable
+{
+	int i;
+	int i_hat;
+	int J;
+	int I;
+};
+
+struct lexiconTable{
+	int src_word;
+	int trg_word;
+};
+
 int main() {
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
